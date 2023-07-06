c3034b115cac3f01979b5edd18941d0c7506c93b
Lukas Klein <lukas.klein@telekom.de>
2023-07-06 12:05:24 +0200

    stable version of scraping

--- scrape.py
+++ scrape.py
@@ -1,7 +1,8 @@
+import os
+import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import time
+from selenium.webdriver import ChromeOptions
 
 class Shop:
     all_shops = []  # Class attribute to store all instances of shops
@@ -15,63 +16,63 @@ class Shop:
     def get_all_shops(cls):
         return cls.all_shops
 
+# Shop instances
+aldi = Shop("Aldi", "https://www.prospektangebote.de/geschaefte/aldi-sud/angebote")
 rewe = Shop("Rewe", "https://www.prospektangebote.de/geschaefte/rewe/angebote")
 lidl = Shop("Lidl", "https://www.prospektangebote.de/geschaefte/lidl/angebote")
-aldi = Shop("Aldi", "https://www.prospektangebote.de/geschaefte/aldi-sued/angebote")
 
 # Accessing all shops
 all_shops = Shop.get_all_shops()
 
-options = Options()
-options.add_argument('--headless')
+options = ChromeOptions()
+options.add_argument("--headless=new")
 
 for shop in all_shops:
-    print("Scraping", shop.name)
+    print(f"Scraping {shop.name}")
+    
+    # Initialize webdriver and navigate to URL
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(30)
     driver.get(shop.url)
 
-    ScrollNumber = 30
-    for i in range(1,ScrollNumber):
+    # Scroll down the page to load content
+    scroll_number = 30
+    for i in range(scroll_number):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight - 2500)")
         time.sleep(2)
 
-    print("done scraping", shop.name)
+    print(f"Done scraping {shop.name}")
 
+    # Save page source to an HTML file
     try:
-        file = open(f'{shop.name}.html', 'w', encoding="utf-8")
-        file.write(driver.page_source)
-        file.close()
-        print("File successfully written.")
+        with open(f'{shop.name}.html', 'w', encoding="utf-8") as file:
+            file.write(driver.page_source)
+        print("HTML File successfully written.")
     except Exception as e:
-        print("An error occurred while writing the file:", str(e))
+        print(f"An error occurred while writing the file: {e}")
     driver.close()
 
+    # Parse HTML content and extract product information
     with open(f'{shop.name}.html', 'r', encoding="utf-8") as file:
         html_content = file.read()
 
     soup = BeautifulSoup(html_content, 'html.parser')
-
-    # Find all elements with the specified class name
     products = soup.find_all(class_='product__bottom')
 
-    # Iterate over the elements and print their text
-    for product in products:
+    # Remove existing text file
+    if os.path.exists(f'{shop.name}.txt'):
+        os.remove(f'{shop.name}.txt')
 
+    for product in products:
         # Extract the name, price, and date using the appropriate CSS selectors
         name = product.select_one('.product__name').get_text(strip=True)
-        
-        price = product.select_one('.product__price-offer').get_text(strip=True) if product.select_one('.product__price-offer') else None
-        if price is None:
-            price = "No price found"
 
-        # Try to select the date with "product-date" class, if not found, try "product__date"
-        date_element = product.select_one('.product-date')
-        if date_element is None:
-            date_element = product.select_one('.product__date')
-        
-        # Extract the date if found, or set it to None otherwise
+        price_element = product.select_one('.product__price-offer')
+        price = price_element.get_text(strip=True) if price_element else "No price found"
+
+        date_element = product.select_one('.product-date') or product.select_one('.product__date')
         date = date_element.get_text(strip=True) if date_element else None
 
+        # Write product information to a text file
         with open(f'{shop.name}.txt', 'a', encoding="utf-8") as file:
             file.write(f'{name}\n{price}\n{date}\n\n')
